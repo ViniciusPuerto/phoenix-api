dc62ce153eee4a3c81807b62d6bb26e0a690e59f
ViniciusPuerto <vinicius.alves.porto@gmail.com>
2020-04-16 19:53:30 -0300

    Better tests

--- lib/my_app/todo/task.ex
+++ lib/my_app/todo/task.ex
@@ -7,15 +7,34 @@ defmodule MyApp.Todo.Task do
     field :completed, :boolean, default: false
     field :description, :string
     field :name, :string
-
-    timestamps()
+    timestamps(usec: false)
   end
 
   @doc false
   def changeset(task, attrs) do
     task
     |> cast(attrs, [:name, :description, :completed])
-    |> validate_required([:name, :description, :completed])
+    |> validate_required([:name, :completed])
     |> unique_constraint(:name)
+    |> validate_length(:name, max: 100, count: :codepoints)
+    |> validate_optional_string(:description, 600, "should be at most 600 characters.")
+  end
+  
+  defp validate_optional_string(changeset, key, max, message) do
+    field = get_field(changeset, key)
+    cond do
+      field == nil ->
+        changeset
+      get_codepoints_length(field) <= max ->
+        changeset
+      true ->
+        add_error(changeset, key, message)
+    end
+  end
+
+  defp get_codepoints_length(field) when is_bitstring(field) do
+      field
+      |> String.codepoints()
+      |> length()
   end
 end
